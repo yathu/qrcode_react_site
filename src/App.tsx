994f94cf0b2bf2d130960a88a5a49dd5b0ead282
yathavan <yathavancom@gmail.com>
2021-09-29 01:16:28 +0530

    basic qrcode generating

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,66 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var QRCode = require("qrcode");
+
 function App() {
-  return (
-    <div className="pt-5 mx-auto px-4 flex-1">
-      <div className="text-center text-gray-300">
-        <h1 className="py-5 my-5">React + TypeScript + TailwindCSS <br /> boilerplate starter project</h1>
-        <h2 className="py-5 text-gray-500">Demo:</h2>
-      </div>
+    const [qrCodeUrl, setQrCodeUrl] = React.useState<any>('');
+    const [qrInputValue, setQrInputValue] = React.useState<any>('');
 
-      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-        <div className="md:flex">
-          <div className="md:flex-shrink-0">
-            <img className="h-48 w-full object-cover md:w-48" src="https://picsum.photos/id/200/300" alt="Man looking at item at a store" />
-          </div>
-          <div className="p-8">
-            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
-            <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
-            <p className="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
-          </div>
+    const GenerateQr = (value: any) => {
+        QRCode.toDataURL(value)
+            .then((url: any) => {
+                console.log("URL ==>", url);
+                setQrCodeUrl(url);
+            })
+            .catch((err: any) => {
+                console.error("Error ==>", err)
+            });
+    };
+
+    const RenderQrCode = useCallback(() => {
+        return <img className="mt-4" src={qrCodeUrl}/>
+    }, [qrCodeUrl]);
+
+
+    const handleChange = (event: any) => {
+        setQrInputValue(event.target.value);
+        GenerateQr(event.target.value);
+    }
+
+    const QrcodeInput = useCallback(() => {
+        return (
+            <form>
+
+                <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    ref={(input) => {
+                        if (input) {
+                            input.focus();
+                        }
+                    }}
+                    type="text" placeholder="URL/Text"
+                    value={qrInputValue} onChange={handleChange}/>
+            </form>
+        );
+    }, [qrInputValue])
+
+    return (
+        <div className="pt-5 mx-auto px-4 flex-1">
+
+            <QrcodeInput/>
+
+            {/*<form>*/}
+            {/*    <input type="text" value={qrInputValue} onChange={handleChange}/>*/}
+            {/*</form>*/}
+
+            {/*{GenerateQr("test Qr")}*/}
+
+            <RenderQrCode/>
+
         </div>
-      </div>
-
-    </div>
-  );
+    );
 }
 
 export default App;
